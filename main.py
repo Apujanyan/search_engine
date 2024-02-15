eeb90fe89e88f647911b897db8fe8db0912166ef
Apujanyan <apujanyan04@gmail.com>
2024-02-15 17:53:20 +0400

    commit

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def file_open(path):
     return file_contents
 
 def main():
+    data = {'1.txt': [],
+            '2.txt': [],
+            '3.txt': []}
+
     while True:
         txt_1 = file_open('1.txt').split(' ')
         txt_2 = file_open('2.txt').split(' ')
@@ -21,16 +25,17 @@ def main():
             continue
         
         for i in range(len(txt_1)):
-            if word in txt_1[i]:
+            if word in txt_1[i] and len(word) == len(txt_1[i]):
                 print(f'\'{word}\' is in 1.txt!')
                 break
         for i in range(len(txt_2)):
-            if word in txt_2[i]:
+            if word in txt_2[i] and len(word) == len(txt_2[i]):
                 print(f'\'{word}\' is in 2.txt!')
                 break
         for i in range(len(txt_3)):
-            if word in txt_3[i]:
+            if word in txt_3[i] and len(word) == len(txt_3[i]):
                 print(f'\'{word}\' is in 3.txt!')
                 break
 
-main()
+if __name__ == '__main__':
+    main()
